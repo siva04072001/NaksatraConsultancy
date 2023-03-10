894e6880e19e474b147e9873b64d0b125172e5eb
Xploits23 <xploits2k23@gmail.com>
2023-03-10 22:04:55 +0530

    first commit

---
+++ helpdesk/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for helpdesk project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpdesk.settings')
+django.setup()
+
+from channels.auth import AuthMiddleware
+from tickets.routing import websocket_urlpatterns
+application = ProtocolTypeRouter({
+    "http" :get_asgi_application(),
+    "websocket" :AuthMiddleware(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )
+    })
